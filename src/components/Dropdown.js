2467636a228761272f2609880b5d47656cc8c23c
Rawand Dargali <rdargali@yahoo.com>
2020-08-20 02:31:01 -0500

    stopwatches running concurrently correctly

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -5,13 +5,18 @@ const Dropdown = ({ setStopwatches }) => {
   return (
     <div>
       <div className="dropdown">
-        <button className="dropbtn">Number of Stopwatches</button>
+        <button className="dropbtn">Stopwatches</button>
         <ul className="dropdown-content">
           <li onClick={() => setStopwatches(1)}>1</li>
           <li onClick={() => setStopwatches(2)}>2</li>
           <li onClick={() => setStopwatches(3)}>3</li>
           <li onClick={() => setStopwatches(4)}>4</li>
           <li onClick={() => setStopwatches(5)}>5</li>
+          <li onClick={() => setStopwatches(6)}>6</li>
+          <li onClick={() => setStopwatches(7)}>7</li>
+          <li onClick={() => setStopwatches(8)}>8</li>
+          <li onClick={() => setStopwatches(9)}>9</li>
+          <li onClick={() => setStopwatches(10)}>10</li>
         </ul>
       </div>
     </div>
